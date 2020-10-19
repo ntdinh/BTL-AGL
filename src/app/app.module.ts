9ad009798fbb9e767cfd025dfa4af799b626494e
ntdinh <70557251+ntdinh@users.noreply.github.com>
2020-10-19 21:19:54 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
+    
   ],
   imports: [
+,
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
